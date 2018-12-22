5b956e3d56f24b4e9c0abbc3b2962aedf7199a7f
Hiroyuki Kuromiya <khiroyuki1993@gmail.com>
2018-12-22 14:25:44 +0900

    cut segment by zero sequence

--- README.md
+++ README.md
@@ -9,6 +9,20 @@ README.md  extractActivity.py         xls2csv.py
 # preprocessing
 ## cut the activity data
 ```
-$python xls2csv.csv
+$python xls2csv.py
 $python extractActivity.py
 ```
+warning: you should correct the typo of year in patient no.1 and no.3 before execute xls2csv.py
+
+## results
+extractActivity.py cut activity data into many segments.
+
+```
+.../MDD_data/Activity_Data/patient1$ ls
+activity_12_0.csv   activity_27_1.csv  activity_42_0.csv  activity_63_1.csv
+activity_12_1.csv   activity_28_1.csv  activity_42_1.csv  activity_65_1.csv ...
+```
+
+file name description: activity_{row number}_{cut_number}
+For example, activity_12_2.csv corresponds to 2nd segment of 12nd row in EMA_data_all.csv for patient no.1 .
+extractActivity cut the segments if 0 continues more than 5 times.
--- extractActivity.py
+++ extractActivity.py
@@ -20,15 +20,32 @@ def main():
         ID = int(re.sub(r'\D', '', fname))
         patient = participants[participants.ID == ID].copy()
         patient['start_time'] = patient.shift().time
+        patient['prevous_schedule'] = patient.shift().schedule
         activity = pd.read_csv(fname, index_col=0, parse_dates=True).sort_index()
         folder = "./MDD_data/Activity_Data/patient{}".format(ID)
         os.makedirs(folder, exist_ok=True)
         for rownum, (key, row) in enumerate(patient.iterrows(), 1):
             activities_cut = extractActivities(activity, row)
-            if len(activities_cut) == 0:
+            # skip sleep period
+            if len(activities_cut) == 0 or row.prevous_schedule == 1:
                 continue
             else:
-                activities_cut.to_csv(folder + "/activity_{}.csv".format(rownum))
+                # cut in zero sequence
+                is_active = activities_cut.apply(lambda x: 0 if x==0 else 1)
+                sequence = is_active.astype(str).str.cat()
+                # thorethold: zero more than 5 times -> device off
+                matches = re.finditer('0{6,}', sequence)
+                start = 0
+                num_off = 0
+                for num_off, match in enumerate(matches):
+                    end = match.start()
+                    # if data lentgh is too short, we don't generate csv file.
+                    if abs(end - start) < 10:
+                        continue
+                    activities_cut[start:end].to_csv(folder + "/activity_{}_{}.csv".format(rownum, num_off))
+                    start = match.end()
+                num_off += 1
+                activities_cut[start:].to_csv(folder + "/activity_{}_{}.csv".format(rownum, num_off))
 
 if __name__ == "__main__":
     main()
